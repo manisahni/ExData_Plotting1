3a1d5ba028802ba2abbfebb3d32326983b5cdf4a
manisahni <nishant.sahni@gmail.com>
2015-09-10 14:58:01 -0500

    plot4

---
+++ plot4.R
@@ -0,0 +1,22 @@
+data <- read.csv("~/Desktop/coursera sept 2015/EDA/household_power_consumption.txt", sep=";")## read in the file
+install.packages("dplyr") # install dplyr
+library(dplyr) # load dplyr
+subset <- filter(data,(Date %in% c("1/2/2007","2/2/2007"))) # select dates
+## fix date time
+
+#str(subSetData)
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+globalReactivePower <- as.numeric(subSetData$Global_reactive_power)
+voltage <- as.numeric(subSetData$Voltage)
+subMetering1 <- as.numeric(subSetData$Sub_metering_1)
+subMetering2 <- as.numeric(subSetData$Sub_metering_2)
+subMetering3 <- as.numeric(subSetData$Sub_metering_3)
+png("plot4.png", width=480, height=480)
+par(mfrow = c(2, 2))
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power", cex=0.2)
+plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
